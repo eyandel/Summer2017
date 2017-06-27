fef05d05cc5e803714583905893de9be89a41abd
eyandel <eyandel16@gmail.com>
2017-06-27 13:56:46 -0400

    Add files via upload

--- myMacro.cxx
+++ myMacro.cxx
@@ -1,4 +1,6 @@
-void myMacro(){
+int myMacro(int k=0){
 	cout << "Trivaial ROOT macro: Hello, World!" << endl;
 	// it sounds like "Cognito, ergo sum" said by HAL9000...
+	cout<< "The input parameter was " << k << endl;
+	return k;
 }
