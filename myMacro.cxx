a591e600c1ec6784783f6562c0da940113247742
eyandel <eyandel16@gmail.com>
2017-06-27 14:03:24 -0400

    Add files via upload

--- myMacro.cxx
+++ myMacro.cxx
@@ -1,6 +1,14 @@
 int myMacro(int k=0){
-	cout << "Trivaial ROOT macro: Hello, World!" << endl;
+	cout << "Trivial ROOT macro: Hello, World!" << endl;
 	// it sounds like "Cognito, ergo sum" said by HAL9000...
 	cout<< "The input parameter was " << k << endl;
 	return k;
+}
+
+double anotherFunc(int j=0){
+ 
+  double pp = 2*acos(-1);
+  double x = 1.5*j + pp*myMacro(j);
+  return x;
+ 
 }
