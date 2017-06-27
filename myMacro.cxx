ce04cd9ebc8ba6d321941877547c86a8b6cf9e2d
eyandel <eyandel16@gmail.com>
2017-06-27 13:39:07 -0400

    Add files via upload

--- myMacro.cxx
+++ myMacro.cxx
@@ -1,6 +1,8 @@
-$ cat myMacro.cxx
+cat myMacro.cxx
+
 void myMacro(){
 	cout << "Trivaial ROOT macro: Hello, World!" << endl;
 	// it sounds like "Cognito, ergo sum" said by HAL9000...
 }
-$ root -q -b myMacro.cxx
+
+root -q -b myMacro.cxx
